399eb6b47a3a345135f03c0c429035c72277a596
hashrock <hashrock@users.noreply.github.com>
2022-01-13 01:40:46 +0900

    share lib test

--- 20220113-packup/frontend/main.ts
+++ 20220113-packup/frontend/main.ts
@@ -1,1 +1,3 @@
 console.log("Hello World");
+import {sum} from "../share/math.ts"
+console.log(sum(1, 2));
--- 20220113-packup/main.ts
+++ 20220113-packup/main.ts
@@ -1,9 +1,11 @@
-import { serve, serveStatic } from "https://deno.land/x/sift@0.4.2/mod.ts";
+import { serve, serveStatic, json } from "https://deno.land/x/sift@0.4.2/mod.ts";
+import { sum } from "./share/math.ts";
 
 serve({
   "/": serveStatic("./dist/index.html", {
     baseUrl: import.meta.url,
   }),
+  "/api": () => json({sum: sum(1, 2)}),
   "/:filename+": serveStatic("./dist", {
     baseUrl: import.meta.url,
   }),
